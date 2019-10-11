b49a52724292f4d58d3a7c4d7bf73a69dcf9a162
Raden Mu'az Mun'im <raden@ks.c.titech.ac.jp>
2019-10-11 15:37:03 +0900

    Pytorch 1.3 8-bit quantization

--- README.md
+++ README.md
@@ -18,13 +18,6 @@ python -u main.py --dataset cifar100 --arch wideresnet --epochs 300 --seed 1 --b
 ```
 Pruned model `pruned_model.pth.tar` is stored in `--save` folder `log-w1`
 
-Run this to quantize.
-```shell
-python quantize_model.py --pruned log-w1/pruned_model.pth.tar --wbit 8 --save log-w1
-
-```
-After quantization, `quantized_model.pth.tar` is stored in `--save` folder `log-w1`.
-
 ## To evaluate models
 - Pretrained models are in `chkpt/` folder.
 - Mult ops and params are halved to be 16-bit with `--freebie`.
@@ -33,50 +26,24 @@ After quantization, `quantized_model.pth.tar` is stored in `--save` folder `log-
 - `compute_flops.py` is the code to calculate FLOPs
 - `base_params = sum(p.numel() for name, p in model.named_parameters())`
 
-8-bit (best score)
-```
-python eval.py --pruned chkpt/quantized_model.pth.tar  --half --nbit 8
+)
 
-Output:
+
+```
+python eval_quantize.py --pruned chkpt/pruned_model.pth.tar
+
 --------------------------------------------------------------------------------
-Freebie 16-bit is False
-Pytorch half-precision is True
-Weight-bit is 8
 params count /  4
 original param count 23181353 @ 23.18 M
 quantized param count 5795338 @ 5.80 M
 original FLOPs count 10625795100.0 @ 10.63 GFLOPs
-FLOPs count 5318413340.0 @ 5.32 GFLOPs
+FLOPs count 2656448775.0 @ 2.66 GFLOPs
 --------------------------------------------------------------------------------
 Reference param count 36500000.0 @ 36.50 M
 Reference FLOPs count 10490000000.0 @ 10.49 GFLOPs
 --------------------------------------------------------------------------------
-Score = Param/refParam + FLOPs/refFLOPs = 0.15878 + 0.50700 = 0.66577
+Score = Param/refParam + FLOPs/refFLOPs = 0.15878 + 0.25324 = 0.41201
 --------------------------------------------------------------------------------
 
-Test set: Average loss: 1.2828, Accuracy: 8028/10000 (80.28%)
-
-```
-
-16-bit freebie
-```
-python eval.py --pruned chkpt/pruned_model.pth.tar  --freebie --half
-
---------------------------------------------------------------------------------
-Freebie 16-bit is True
-Pytorch half-precision is True
-Weight-bit is 32
-params count /  2
-original param count 23181353 @ 23.18 M
-quantized param count 11590676 @ 11.59 M
-original FLOPs count 10625795100.0 @ 10.63 GFLOPs
-FLOPs count 5318413340.0 @ 5.32 GFLOPs
---------------------------------------------------------------------------------
-Reference param count 36500000.0 @ 36.50 M
-Reference FLOPs count 10490000000.0 @ 10.49 GFLOPs
---------------------------------------------------------------------------------
-Score = Param/refParam + FLOPs/refFLOPs = 0.31755 + 0.50700 = 0.82455
---------------------------------------------------------------------------------
-
-Test set: Average loss: 1.2811, Accuracy: 8046/10000 (80.46%)
+Test set: Average loss: 1.2810, Accuracy: 8044/10000 (80.44%)
 ```
---
+++ eval_quantize.py
@@ -0,0 +1,121 @@
+from __future__ import print_function
+import os
+import argparse
+import shutil
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+from compute_flops import print_model_param_flops
+import torch.quantization
+import models
+import time
+import numpy as np
+REF_PARAM=36.5e6
+REF_FLOPS=10.49e9
+# Training settings
+parser = argparse.ArgumentParser(description='PyTorch Slimming CIFAR training')
+parser.add_argument('--dataset', type=str, default='cifar100',
+                    help='training dataset (default: cifar100)')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='disables CUDA training')
+parser.add_argument('--seed', type=int, default=1, metavar='S',
+                    help='random seed (default: 1)')
+parser.add_argument('--pruned', default='', type=str, metavar='PATH',
+                    help='path to latest checkpoint (default: none)')
+parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                    help='input batch size for training (default: 64)')
+parser.add_argument('--freebie', action='store_true', default=False,
+                    help='16-bit')
+parser.add_argument('--half', action='store_true', default=False,
+                    help='half-precision')
+parser.add_argument('--nbit', type=int, default=32, metavar='N',
+                    help='weight bit')
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+
+torch.manual_seed(args.seed)
+if args.cuda:
+    torch.cuda.manual_seed(args.seed)
+
+kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
+if args.dataset == 'cifar10':
+
+    test_loader = torch.utils.data.DataLoader(
+        datasets.CIFAR10('./data.cifar10', train=False, transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+                       ])),
+        batch_size=args.batch_size, shuffle=True, **kwargs)
+else:
+
+    test_loader = torch.utils.data.DataLoader(
+        datasets.CIFAR100('./data.cifar100', train=False, transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+                       ]), download=True),
+        batch_size=args.batch_size, shuffle=True, **kwargs)
+
+assert args.pruned
+def run(a,b):pass
+model = torch.load(args.pruned, map_location='cpu')
+
+# if args.cuda:
+#     model.cuda()
+base_flops = print_model_param_flops(model, 32, multiply_adds=True, freebie=False) # only either 32 or 16 bit
+
+torch.quantization.quantize(model, run, None)
+
+print(model)
+print("-"*80)
+base_params = sum(p.numel() for name, p in model.named_parameters())
+
+mult = 1
+args.nbit=8
+mult = 32//8
+print("params count / ", mult)
+params = base_params // mult
+print("original param count", base_params, '@ {:.2f} M'.format(base_params/1e6))
+print("8-bit quantized param count", params, '@ {:.2f} M'.format(params/1e6))
+flops = base_flops//mult # only either 32 or 16 bit
+print("original FLOPs count", base_flops, '@ {:.2f} GFLOPs'.format(base_flops/1e9))
+print("8-bit FLOPs count", flops, '@ {:.2f} GFLOPs'.format(flops/1e9))
+print("-"*80)
+print("Reference param count", REF_PARAM, '@ {:.2f} M'.format(REF_PARAM/1e6))
+print("Reference FLOPs count", REF_FLOPS, '@ {:.2f} GFLOPs'.format(REF_FLOPS/1e9))
+param_score = params/REF_PARAM
+flops_score = flops/REF_FLOPS
+score = param_score + flops_score
+print("-"*80)
+print("Score = Param/refParam + FLOPs/refFLOPs = {:.5f} + {:.5f} = {:.5f}".format(param_score, flops_score, score))
+print("-"*80)
+if args.half:
+    model = model.half()
+def test():
+    model.eval()
+    test_loss = 0
+    correct = 0
+    for i, (data, target) in enumerate(test_loader):
+        # if args.cuda:
+        #     data, target = data.cuda(), target.cuda()
+        # if args.half:
+        #     data = data.half()
+        output = model(data)
+        loss = F.cross_entropy(output, target, size_average=False).item() # sum up batch loss
+        test_loss += loss
+        pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
+        correct += pred.eq(target.data.view_as(pred)).cpu().sum().item()
+        print(i, loss)
+
+    test_loss /= len(test_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    return float(correct) / float(len(test_loader.dataset))
+
+
+
+test()
